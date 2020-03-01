d6f8a97f31141adfe10cda8c95940588eec375c8
Max <angrymaks.2@gmail.com>
2020-03-01 18:35:59 +0300

    heap finished

--- 1l_priority_queue/priority_queue.py
+++ 1l_priority_queue/priority_queue.py
@@ -1,42 +1,27 @@
 
 class Heap:
 
-    def __init__(self, heap=[]):
+    def __init__(self, order, heap=[]):
+        if order not in (min, max):
+            raise NameError("Incorrect order. Use 'min' or 'max'.")
+
         self.heap = heap
-        if len(self.heap) != 0:
-            self.heapify(len(self.heap) - 1)
+        self.order = order
 
-    def push(self):
-        pass
+        if self.heap:
+            self.heapify()
 
-    def pop(self):
-        pass
+    def __str__(self):
+        return str(self.heap)
 
-    def heapify(self, last_index):
-        start_index = self.parent_of(last_index)
+    def __len__(self):
+        return len(self.heap)
 
-        for i in range(start_index, -1, -1):
-            self.sift_down(i, last_index)
+    def in_order(self, parent, child):
+        return self.order(parent, child) == parent
 
-    def sift_down(self, start, last_index):
-        current = start
-        print(self.heap)
-        while self.left_child_of(current) <= last_index:
-            left_child = self.left_child_of(current)
-            right_child = self.right_child_of(current)
-            swapped = current
-
-            if self.heap[left_child] > self.heap[swapped]:
-                swapped = left_child
-
-            if right_child <= last_index and self.heap[right_child] > self.heap[swapped]:
-                swapped = right_child
-
-            if swapped == current:
-                return
-            else:
-                self.swap(current, swapped)
-                current = swapped
+    def peek(self):
+        return self.heap[0]
 
     def parent_of(self, i):
         return (i - 1) // 2
@@ -48,10 +33,63 @@ class Heap:
         return 2 * i + 2
 
     def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
-        tmp = self.heap[i]
-        self.heap[i] = self.heap[j]
-        self.heap[j] = tmp
+    def empty(self):
+        return len(self.heap) == 0
+
+    def hpush(self, value):
+        self.heap.append(value)
+        self.sift_up(len(self.heap) - 1)
+
+    def hpop(self):
+        if self.heap:
+            first = self.heap[0]
+            self.swap(0, -1)
+            del self.heap[-1]
+            self.sift_down(0)
+            return first
+        else:
+            raise Exception("Heap is empty")
+
+    def heapify(self):
+        parent_of_last = self.parent_of(len(self.heap) - 1)
+        parents = range(parent_of_last, -1, -1)
+
+        for parent in parents:
+            self.sift_down(parent)
+
+    def sift_up(self, current):
+        in_order = self.in_order
+        heap = self.heap
+        parent = self.parent_of(current)
+        while current > 0 and not in_order(heap[parent], heap[current]):
+            self.swap(current, parent)
+            current = parent
+            parent = self.parent_of(current)
+
+    def sift_down(self, current):
+        size = len(self.heap)
+        in_order = self.in_order
+        heap = self.heap
+
+        while self.left_child_of(current) < size:
+            left_child = self.left_child_of(current)
+            right_child = self.right_child_of(current)
+            swapped = current
+
+            if not in_order(heap[swapped], heap[left_child]):
+                swapped = left_child
+
+            if right_child < size and \
+                    not in_order(heap[swapped], heap[right_child]):
+                swapped = right_child
+
+            if swapped == current:
+                return
+            else:
+                self.swap(current, swapped)
+                current = swapped
 
 
 class PriorityQueue:
@@ -60,5 +98,7 @@ class PriorityQueue:
 
 
 if __name__ == '__main__':
-    heap = Heap([4, 5, 1, 2, 8, 0, 9])
-    print(heap.heap)
+    heap1 = Heap(list, [4, 5, 1, 2, 8, 0, 9])
+    print(heap1)
+    heap1.hpush(10)
+    print(heap1)
