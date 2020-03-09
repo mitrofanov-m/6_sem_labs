8d7e55f192907e61382b224bea95ed52b80cb79d
Max <angrymaks.2@gmail.com>
2020-03-10 02:02:52 +0300

    started AVL class + simple test

--- 2l_AVL_map/AVLTree.py
+++ 2l_AVL_map/AVLTree.py
@@ -5,48 +5,142 @@ class Node:
         self.left = None
         self.right = None
 
+    # thanks to classmate for this feature
+    def __str__(self):
+        return self.diagram(self, "", "", "")
+
+    def diagram(self, node, top, root, bottom):
+        if not node:
+            return root + "nil\n"
+        if node.left is None and node.right is None:
+            return root + " " + str(node.value) + "\n"
+        return self.diagram(node.right, top + " ", top + "┌──", top + "│ ") + \
+               root + str(node.value) + "\n" + \
+               self.diagram(node.left, bottom + "│ ", bottom + "└──", bottom + " ")
+
 
 class BinaryTree:
     def __init__(self, Node=Node):
-        self.root = None
-        self.count = 0
-        self.Node = Node
+        self.__root = None
+        self.__count = 0
+        self.__Node = Node
+
+    def __str__(self):
+        return str(self.__root)
 
     # Public Methods Section #
     def insert(self, value):
-        self._insert_in(self, self.root, value)
+        self.__root = self._insert_in(self.__root, value)
 
     def contains(self, value):
-        return self._contains_in(self.root, value)
+        return self._contains_in(self.__root, value)
 
     def remove_all(self):
         self._post_order_removing()
-        self.count = 0
+        self.__count = 0
+
+    def in_order(self):
+        traverse_list = []
+        self._in_order(self.__root, traverse_list)
+        return traverse_list
 
     # Private Methods Section #
-    def _insert_in(self, current_node, value):
-        if not current_node:
-            self.count += 1
-            return self.Node(value)
-        elif value < current_node.value:
-            current_node.left = self._insert_in(current_node.left, value)
+    def _insert_in(self, node, value):
+        if not node:
+            self.__count += 1
+            return self.__Node(value)
+        elif value < node.value:
+            node.left = self._insert_in(node.left, value)
         else:
-            current_node.right = self._insert_in(current_node.right, value)
+            node.right = self._insert_in(node.right, value)
 
-        return current_node
+        return node
 
-    def _contains_in(self, current_node, value):
-        if not current_node:
+    def _contains_in(self, node, value):
+        if not node:
             return False
-        elif value < current_node.value:
-            return self._contains_in(current_node.left, value)
-        elif value > current_node.value:
-            return self._contains_in(current_node.right, value)
+        elif value < node.value:
+            return self._contains_in(node.left, value)
+        elif value > node.value:
+            return self._contains_in(node.right, value)
         else:
             return True
 
-    def _post_order_removing(self, current_node):
-        if current_node:
-            current_node.left = self._post_order_removing(current_node.left)
-            current_node.right = self._post_order_removing(current_node.right)
+    def _in_order(self, node, traverse_list):
+        if node:
+            self._in_order(node.left, traverse_list)
+            traverse_list.append(node.value)
+            self._in_order(node.right, traverse_list)
+
+    def _post_order_removing(self, node):
+        if node:
+            node.left = self._post_order_removing(node.left)
+            node.right = self._post_order_removing(node.right)
+            node.value = None
         return None
+
+
+class AVLNode(Node):
+    def __init__(self, value):
+        super().__init__(value)
+        self.height = 1
+
+
+class AVLTree(BinaryTree):
+    def __init__(self):
+        super().__init__(AVLNode)
+
+    # Private Methods Section #
+    def _insert_in(self, node, value):
+        node = super()._insert_in(node, value)
+        # append balancing of node that is returned
+        return self._balanced(node)
+
+    def _get_height(self, node):
+        if not node:
+            return -1
+
+        return node.height
+
+    def _refresh_height(self, node):
+        return 1 + max(self._get_height(node.left),
+                       self._get_height(node.right))
+
+    def _right_rotate(self, node):
+        pivot = node.left
+        node.left = pivot.right
+        pivot.right = node
+
+        node.height = self._refresh_height(node)
+        pivot.height = self._refresh_height(pivot)
+
+        return pivot
+
+    def _left_rotate(self, node):
+        pivot = node.right
+        node.right = pivot.left
+        pivot.left = node
+
+        node.height = self._refresh_height(node)
+        pivot.height = self._refresh_height(pivot)
+
+        return pivot
+
+    def _get_balance(self, node):
+        if not node:
+            return 0
+        return self._get_height(node.right) - self._get_height(node.left)
+
+    def _balanced(self, node):
+        if self._get_balance(node) == 2:
+            if self._get_balance(node.right) == -1:
+                node.right = self._right_rotate(node.right)
+            return self._left_rotate(node)
+
+        elif self._get_balance(node) == -2:
+            if self._get_balance(node.left) == 1:
+                node.left = self._left_rotate(node.left)
+            return self._right_rotate(node)
+        else:
+            node.height = self._refresh_height(node)
+            return node
---
+++ 2l_AVL_map/test.py
@@ -0,0 +1,11 @@
+from AVLTree import *
+
+tree = AVLTree()
+tree.insert(0)
+tree.insert(5)
+tree.insert(6)
+tree.insert(7)
+tree.insert(8)
+tree.insert(9)
+tree.insert(-1)
+print(tree)
