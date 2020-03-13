538d1944c32bd8c35fefa264c15223fb75a3bad9
Max <angrymaks.2@gmail.com>
2020-03-14 01:36:12 +0300

    added more load test

--- 2l_AVL_map/test.py
+++ 2l_AVL_map/test.py
@@ -1,9 +1,20 @@
 from AVLTree import *
 from Map import Map
 from math import log2
+from time import time
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+sys.setrecursionlimit(1500) 
 
 
-print("\n------- 1 Test Tree --------\n")
+str0 = \
+"""
+------------ 1 Test Tree ------------
+
+"""
+print(str0)
 
 avltree = AVLTree()
 bintree = BinaryTree()
@@ -25,25 +36,81 @@ print(f"\nLen of AVL tree is: {len(avltree)}")
 print(f"Binary tree contains value = 0 : {0 in bintree}")
 print(avltree.get_height())
 
-print("""------- 2 Test Tree --------
 
-Test the worst case for the binary tree,
-appending in order 0,1...n\n""")
+str1 = \
+"""
+------------ 2 Test Tree --------------
 
-avltree.remove_all()
-bintree.remove_all()
+Test the worst case for the binary tree:
+    - appending in order
+    - searching in order
+"""
+print(str1)
 
-load = 800
-for value in range(load):
-    avltree.insert(value)
-    bintree.insert(value)
+def insert_timetest(tree, load, step):
+    test = []
+    len_of_tree = []
+    for value in range(0,load+1):
+        start = time()
+        tree.insert(value)
+        stop = time()
+        if value % step == 0:
+            test.append(stop - start)
+            len_of_tree.append(value)
 
-print(f"Height of binary tree : {len(bintree)}")
-print(f"Height of AVL tree    : {avltree.get_height()}")
-print(f"log2({load}) : {log2(load)}")
+    return test, len_of_tree
 
-# print("AVL tree:", avltree, sep='\n')
-# print("Binary tree:", bintree, sep='\n')
+def contains_timetest(tree, load, step):
+    test = []
+    len_of_tree = []
+    for value in range(0,load+1):
+        tree.insert(value)
+
+    for value in range(0,load+1):
+        start = time()
+        result = value in tree
+        stop = time()
+        if value % step == 0:
+            test.append(stop - start)
+            len_of_tree.append(value)
+
+    return test, len_of_tree
+
+def mean_of(func, TreeClass, load, step):
+    tests = []
+    for i in range(10):
+        tree = TreeClass()
+        tmp, len_of_tree = func(tree, load, step)
+        tests.append(tmp)
+
+    return np.mean(tests, axis=0), len_of_tree
+
+
+def plotting(title, func, load, step):
+    avltime, len_of_tree = mean_of(func, AVLTree, load, step)
+    bintime, len_of_tree = mean_of(func, BinaryTree, load, step)
+    
+    plt.plot(len_of_tree, avltime, label='AVL tree')
+    plt.plot(len_of_tree, bintime, label='Binary tree')
+    plt.title(title)
+    plt.xlabel('len of tree')
+    plt.ylabel('time')
+    plt.legend()
+    plt.show()
+
+    return avltime, bintime, len_of_tree
+
+avltime, bintime, len_of_tree = \
+    plotting('Speed test of appending in worst case', insert_timetest, load=300, step=25)
+
+for i in range(len(avltime)-1):
+    if avltime[i] > bintime[i] and avltime[i+1] <= bintime[i+1]:
+        print(f"AVL benefits starting between steps {len_of_tree[i]} - {len_of_tree[i+1]}")
+        break 
+else:
+    print("AVL tree slower than simple Binary tree")
+
+plotting('Speed test of searching in worst case', contains_timetest, load=1000, step=100)
 
 
 print("\n\n------- Test Map --------\n")
@@ -52,6 +119,7 @@ mmap = Map()
 mmap[1] = "my"
 mmap[2] = "friend"
 mmap[0] = "hello"
+mmap[1] = "ooops"
 
 del mmap[0]
 print(f"mmap = {mmap}")
